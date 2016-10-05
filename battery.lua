4b92534dcc3f022189e65bb3350996ae442bb558
Zach Collins <collinzy@mail.uc.edu>
2016-10-05 13:51:08 -0400

    maybe made the battery text red when below 20. maybe not. gotta wait to find out.

--- battery.lua
+++ battery.lua
@@ -6,7 +6,14 @@ batterywidgettimer = timer({ timeout = 30 })
 batterywidgettimer:connect_signal("timeout",
     function()
         fh = assert(io.popen("acpi | cut -d, -f 2,2 -", "r"))
-        batterywidget:set_text("Battery:" .. fh:read("*l"))
+        batStr = fh:read("*l")
+        batNum = string.match(batStr, "%d..")
+        batNum = string.gsub(batNum, "%%", "")
+        if (batNum == nil or batNum < 21) then
+            batterywidget:set_markup( '<span color="red">Battery:' .. batStr .. '</span>' )
+        else
+            batterywidget:set_text("Battery:" .. batStr)
+        end
         fh:close()
     end
 )
